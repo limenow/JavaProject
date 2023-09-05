ed338d2fbf464e9e6472f94abd20f6e7d6692fff
limenow <ime2me12@naver.com>
2023-09-06 01:07:17 +0900

    First commit (기본 생성자와 함수 추가)

---
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -0,0 +1,23 @@
+package com.mycom.word;
+public class WordCRUD implements ICRUD{
+    @Override
+    public Object add() {
+        return null;
+    }
+    public void addWord(){}
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+    public void updateWord(){}
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+    public void deleteWord(){}
+    @Override
+    public void selectOne(int id) {
+
+    }
+    public void listAll(){}
+}
